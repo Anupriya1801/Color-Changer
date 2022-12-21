95c78f72aef442bb7236d43520314e2a7a180362
Anupriya1801 <anuarthi4545@gmail.com>
2022-12-21 11:22:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+import Changecolor  from './Components/Changecolor';
+function App(){
+  return(
+    <div>
+      <Changecolor/>
+    </div>
+  )
+}
+export default App;
